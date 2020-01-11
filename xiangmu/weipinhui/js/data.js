f0bd7cf4c0b5f9fc0b8e9a09fe26fc7d1cd23072
tingzhangzuida <59728744+tingzhangzuida@users.noreply.github.com>
2020-01-11 18:02:10 +0800

    Add files via upload

---
+++ xiangmu/weipinhui/js/data.js
@@ -0,0 +1,61 @@
+var books = [{
+    src: "https://h2.appsimg.com/a.appsimg.com/upcb/2019/11/30/181/ias_157510115312463.jpg.webp",
+    title: "逸阳ESEY女裤-新春特卖3折封顶",
+    price: "3折封顶",
+    link: "立即抢购"
+},
+{
+    src: "https://h2.appsimg.com/a.appsimg.com/upcb/2020/01/06/188/ias_157827483692726.jpg.webp",
+    title: "IHIMI女装-新春特卖专场",
+    price: "3折封顶",
+    link: "立即抢购"
+},
+{
+    src: "https://h2.appsimg.com/a.appsimg.com/upcb/2020/01/07/188/ias_157836569184132.jpg.webp",
+    title: "拉谷谷lagogo新春特卖女装专场",
+    price: "0.7折起",
+    link: "立即抢购"
+},
+{
+    src: "https://h2.appsimg.com/a.appsimg.com/upcb/2020/01/06/163/ias_157830517864264.jpg.webp",
+    title: "尚都比拉SENTUBILA女装--新春特卖3折封顶",
+    price: "3折封顶",
+    link: "立即抢购"
+},
+{
+    src: "https://h2.appsimg.com/a.appsimg.com/upcb/2020/01/03/144/ias_157801580593490.jpg.webp",
+    title: "ONE MORE女装-新春限时特卖",
+    price: "4.9折封顶",
+    link: "立即抢购"
+},
+{
+    src: "https://h2.appsimg.com/a.appsimg.com/upcb/2020/01/03/66/ias_157804299180446.jpg.webp",
+    title: "乐町女装-新春限时特卖",
+    price: "1.1折起",
+    link: "立即抢购"
+},
+{
+    src: "https://h2.appsimg.com/a.appsimg.com/upcb/2020/01/03/28/ias_157802016868591.jpg.webp",
+    title: "索菲丝尔SOFSIL女裤-新春特卖3折封顶",
+    price: "3折封顶",
+    link: "立即抢购"
+},
+{
+    src: "https://h2.appsimg.com/a.appsimg.com/upcb/2020/01/06/122/ias_157830859396327.jpg.webp",
+    title: "福玛玛女装-新春特卖3折封顶",
+    price: "3折封顶",
+    link: "立即抢购"
+},
+{
+    src: "https://h2.appsimg.com/a.appsimg.com/upcb/2020/01/02/197/ias_157795189432784.jpg.webp",
+    title: "七格格旗下品牌女装-新春特卖3折封顶",
+    price: "3折封顶",
+    link: "立即抢购"
+},
+{
+    src: "https://h2.appsimg.com/a.appsimg.com/upcb/2019/11/06/14/ias_157300652975298.jpg.webp",
+    title: "初语女装-新春限时特卖",
+    price: "3.1折封顶",
+    link: "立即抢购"
+}];
+
